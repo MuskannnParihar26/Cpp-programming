711364618be12438ee3edd680d19b5ae94dd2824
bughuntermuskan26 <muskanparihar26@gmail.com>
2026-03-21 23:20:22 +0530

    Basic code

---
+++ swap.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"enter the value of a & b";
+    cin>>a>>b;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<"swapping is="<<a<<b;
+
+}
